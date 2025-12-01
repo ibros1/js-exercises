acf4e854b10df4fe6be359ca3780c9b3e11308b2
ibros1 <maxamedsuhayb41@gmail.com>
2025-12-01 17:13:43 +0300

    pushed exercise #3

---
+++ exercise3/main.js
@@ -0,0 +1,6 @@
+const isSunny = true;
+const isWeakend = false;
+
+const canGoBeach = isSunny && isWeakend;
+console.log(canGoBeach + "yeah you can go at the moment!");
+console.log(isSunny || isWeakend);
