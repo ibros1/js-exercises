8df71bc1fb8a2a8d1c2f99e9266e838a69e08bb4
ibros1 <maxamedsuhayb41@gmail.com>
2025-12-13 14:22:48 +0300

    added exercise #4

--- exercise3/main.js
+++ exercise3/main.js
@@ -1,6 +1,6 @@
-const isSunny = true;
-const isWeakend = false;
+// const isSunny = true;
+// const isWeakend = false;
 
-const canGoBeach = isSunny && isWeakend;
-console.log(canGoBeach + "yeah you can go at the moment!");
-console.log(isSunny || isWeakend);
+// const canGoBeach = isSunny && isWeakend;
+// console.log(canGoBeach + "yeah you can go at the moment!");
+// console.log(isSunny || isWeakend);
---
+++ exercise4/app.js
@@ -0,0 +1,13 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const result = add(10, 20);
+// console.log(result);
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const result = add(10, 20);
+console.log(result);
