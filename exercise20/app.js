ebf6fa3e620a996f34d13802bc3f2b469a234c0d
ibros1 <maxamedsuhayb41@gmail.com>
2025-12-30 14:32:21 +0300

    uploaded ex #19 - #21

---
+++ exercise20/app.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "avacado"];
+
+console.log("apple length is" + " " + fruits[0].length);
+console.log("banana length is" + " " + fruits[1].length);
+console.log("avacado length is" + " " + fruits[2].length);
---
+++ exercise21/main.js
@@ -0,0 +1,4 @@
+const array = [1, 2, 3, 4, 5];
+
+const result = array.reduce((total, num) => total * num, 1);
+console.log(result);
